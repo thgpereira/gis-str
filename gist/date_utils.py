ec097c542d83f42d31f5f9e41b918e8782d84c7a
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2020-04-29 23:32:37 -0300

    Adiciona testes unitários

--- gist/date_utils.py
+++ gist/date_utils.py
@@ -2,10 +2,11 @@ import datetime
 import time
 
 class DateUtis:
+    @staticmethod
     def convert_datetime_to_timestamp(datetime_convert, datetime_format):
         try:
             date = datetime.datetime.strptime(datetime_convert, datetime_format)
-            local_time = date.astimezone().timetuple()
+            local_time = date.timetuple()
             return time.mktime(local_time)
         except:
             raise ValueError("Error convert datetime to timestamp") 
